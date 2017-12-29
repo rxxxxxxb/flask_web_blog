eb5a36e0ec4de6a7a9a68cdaa01ff108c4903ae3
rkib <rkib>
2017-12-29 19:54:04 +0600

    minor UI

--- src/app.py
+++ src/app.py
@@ -84,6 +84,20 @@ def create_new_blog():
     return make_response(user_blogs(user._id))
 
 
+@app.route('/posts/new/<string:blog_id>', methods=['POST', 'GET'])
+def create_new_post(blog_id):
+    if request.method == 'GET':
+        return render_template('new_post.html', blog_id = blog_id)
+    else:
+        title = request.form['title']
+        content = request.form['content']
+        user = User.get_by_email(session['email'])
+
+        new_post = Post(blog_id, title, content, user.email)
+        new_post.save_to_mongo()
+
+    return make_response(blog_posts(blog_id))
+
 
 
 if __name__ == '__main__':
